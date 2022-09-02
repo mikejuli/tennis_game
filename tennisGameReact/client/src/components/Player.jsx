e8af877258ec2bd534bdc8e3bfed47cacd8eff89
Mikhail <juliinstant88@gmail.com>
2022-09-02 00:49:14 -0700

    fixed sounds in the main menu

--- tennisGameReact/client/src/components/Player.jsx
+++ tennisGameReact/client/src/components/Player.jsx
@@ -1,23 +1,42 @@
 import React, { useState, useEffect } from "react";
+import {useSelector} from 'react-redux'
 
 const useAudio = url => {
   const [audio] = useState(new Audio(url));
-  const [playing, setPlaying] = useState(false);
+  const [playing, setPlaying] = useState(true);
 
   const toggle = () => setPlaying(!playing);
+  const soundRedux = useSelector((state)=>state)
+
+
+  useEffect(()=>{
+
+    setPlaying(soundRedux.sound.value);
+
+  })
 
   useEffect(() => {
+
+
       playing ? audio.play() : audio.pause();
+
+
+   return () => { audio.pause();}
+
+
+
     },
     [playing]
   );
 
-  useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false));
-    return () => {
-      audio.removeEventListener('ended', () => setPlaying(false));
-    };
-  }, []);
+  // useEffect(() => {
+  //   audio.addEventListener('ended', () => setPlaying(false));
+  //   return () => {
+  //     audio.pause();
+  //     console.log('from removeeventlistener')
+  //     audio.removeEventListener('ended', () => setPlaying(false));
+  //   };
+  // }, []);
 
   return [playing, toggle];
 };
@@ -25,11 +44,11 @@ const useAudio = url => {
 const Player = ({ url }) => {
   const [playing, toggle] = useAudio(url);
 
-  return (
-    <div style = {{position: 'absolute'}}>
-      <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
-    </div>
-  );
+  // return (
+  //   <div style = {{position: 'absolute'}}>
+  //     <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+  //   </div>
+  // );
 };
 
 export default Player;
